eb6cb09b2a762b8720ba845838a5b927aba06f09
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-17 12:51:40 +0700

    added models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from main.models import Category, Product
+
+
 # Register your models here.
+
+@admin.register(Category)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    search_fields = ('name', 'description', )
+
+
+@admin.register(Product)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', )
+    list_filter = ('category', )
+    search_fields = ('name', 'description', 'category', )
