ce762e22893656cde310acf371e119f98c70286c
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-28 22:41:56 +0700

    refactoring: rename admin models

--- store_app/admin.py
+++ store_app/admin.py
@@ -6,19 +6,19 @@ from store_app.models import Category, Product, Contact
 # Register your models here.
 
 @admin.register(Category)
-class StudentAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
     search_fields = ('name', 'description',)
 
 
 @admin.register(Product)
-class StudentAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'category', 'price',)
     list_filter = ('category',)
     search_fields = ('name', 'description', 'category',)
 
 
 @admin.register(Contact)
-class StudentAdmin(admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'order', 'address',)
     search_fields = ('name', 'address',)
