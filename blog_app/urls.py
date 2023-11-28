4c4e9477239886d9f6bc64ede85d3ab0f4ca2ea7
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-29 01:20:40 +0700

    add Article ListView

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from blog_app.apps import BlogAppConfig
+from blog_app.views import ArticleListView
 
 app_name = BlogAppConfig.name
 
 urlpatterns = [
-    # path('articles/')
+    path('articles/', ArticleListView.as_view(), name='articles'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from blog_app.models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(is_published=True).order_by('-create_date')
+
+        return qs
+
