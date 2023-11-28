0416d4f77442223300149db8892addc808d1f40f
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-29 04:20:41 +0700

    add Article DetailView

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog_app.apps import BlogAppConfig
-from blog_app.views import ArticleListView
+from blog_app.views import ArticleListView, ArticleDetailView
 
 app_name = BlogAppConfig.name
 
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='articles'),
+    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
 from blog_app.models import Article
 
@@ -12,3 +13,18 @@ class ArticleListView(ListView):
 
         return qs
 
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = self.object.title
+        ctx['popular'] = Article.objects.filter(is_published=True).filter(~Q(id=self.object.id)).order_by('-view_count')[:3]
+        return ctx
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
