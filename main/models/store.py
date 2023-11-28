37edc2f5c65223f81959a1a1cc676870acda9218
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-28 19:53:10 +0700

    refactoring: move models to package

--- main/models/store.py
+++ main/models/store.py
@@ -2,19 +2,13 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+from .const import NULLABLE
 from main.utils.HashStorage import product_preview_images, HashStorage
-
-# Create your models here.
-
-nullable = {
-    'null': True,
-    'blank': True
-}
 
 
 class Category(models.Model):
     name = models.CharField(max_length=250, verbose_name='наименование')
-    description = models.TextField(max_length=1024, verbose_name='описание', **nullable)
+    description = models.TextField(max_length=1024, verbose_name='описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}: {self.description[:15]}' if self.description else self.name
@@ -27,13 +21,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=250, verbose_name='наименование')
-    description = models.TextField(max_length=1024, verbose_name='описание', **nullable)
+    description = models.TextField(max_length=1024, verbose_name='описание', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
     price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='цена', default=0.0)
     create_date = models.DateTimeField(verbose_name='дата создания', default=timezone.now)
     modify_date = models.DateTimeField(verbose_name='дата изменения', default=timezone.now)
     preview_image = models.ImageField(upload_to=product_preview_images, storage=HashStorage(),
-                                      verbose_name='изображение', **nullable)  # 'product_preview_images/'
+                                      verbose_name='изображение', **NULLABLE)  # 'product_preview_images/'
 
     def __str__(self):
         return f'{self.name}: {self.price:.3}'
