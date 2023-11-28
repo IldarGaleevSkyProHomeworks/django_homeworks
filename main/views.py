b854ee99f8095bed985c46b1a8b6bd777d5c65d8
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-11-28 20:00:18 +0700

    refactoring: remove feedback

--- main/views.py
+++ main/views.py
@@ -54,19 +54,7 @@ def product_info(request, product_id):
     return redirect(catalog, permanent=True)
 
 
-in_memory_db = []
-
-
 def contacts(request: WSGIRequest):
-    if request.method == 'POST':
-        new_item = {
-            'name': request.POST.get('name'),
-            'email': request.POST.get('email'),
-            'message': request.POST.get('message'),
-        }
-        in_memory_db.append(new_item)
-        pprint(new_item)
-
     contact_list = [{
         'name': contact.name,
         'phones': [phone.strip() for phone in contact.phones.split(',')],
@@ -78,7 +66,6 @@ def contacts(request: WSGIRequest):
         'main/contacts.html',
         {
             'title': 'Контакты',
-            'feedback': [{'name': f'{item["name"][0]}***', 'message': item["message"]} for item in in_memory_db[-3:]],
             'contacts': contact_list
         })
 
